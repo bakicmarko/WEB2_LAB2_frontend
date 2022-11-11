044b28ba01967ecbd29f81dec5fcfbd98cc4298d
Marko Bakic <marko.bakic@fer.hr>
2022-11-11 19:39:07 +0100

    dasasddsaddsadasd

--- src/components/constant/constants.js
+++ src/components/constant/constants.js
@@ -3,4 +3,4 @@ const dev_url = "http://localhost:5000";
 const prod_url = "https://web2-lab-backend.onrender.com";
 
 export const baseUrl =
-  process.env.ENVIRONMENT === "production" ? prod_url : dev_url;
+  process.env.REACT_APP_ENVIRONMENT === "production" ? prod_url : dev_url;
